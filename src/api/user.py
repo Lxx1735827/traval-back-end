b05718ba6cec046e5cdfcb194b1c9902a237bd32
Lxx1735827 <870375416@qq.com>
2024-10-07 18:52:05 +0800

    user接口

--- src/api/user.py
+++ src/api/user.py
@@ -16,7 +16,7 @@ async def add_user(new_user: UserSchema):
     user_exist = await User.get_or_none(number=new_user.number)
     # 如果用户不存在，则创建新用户
     if user_exist is not None:
-        raise HTTPException(status_code=400, detail="User with this email already exists.")
+        raise HTTPException(status_code=400, detail="User with this phone number already exists.")
     # 验证码 TODO
 
     # 插入数据库
@@ -28,18 +28,14 @@ async def add_user(new_user: UserSchema):
 async def update_user(new_user: UserSchema):
     # 查看是否已经存在
     user_exist = await User.get_or_none(number=new_user.number)
-
     # 如果用户不存在，返回 404 错误
     if user_exist is None:
         raise HTTPException(status_code=404, detail="User not found.")
-
     # 更新用户信息
     user_exist.password = new_user.password
     user_exist.avatar = new_user.avatar
-
     # 保存更新
     await user_exist.save()
-
     return {"data": "用户信息更新成功"}
 
 
