0edb5e824cfb1a0c4b5784fe1b14e4c4895610c7
Lxx1735827 <870375416@qq.com>
2024-10-08 17:41:19 +0800

    user接口修改

--- src/api/user.py
+++ src/api/user.py
@@ -10,11 +10,18 @@ async def get_all_users():
     return
 
 
+@user.get("/{user_number}", description="得到一个用户的所有信息")
+async def get_user(user_number:str):
+    user_exist = await User.get_or_none(number=user_number)
+    if user_exist is not None:
+        raise HTTPException(status_code=400, detail="User with this phone number already exists.")
+    return {"data": user_exist}
+
+
 @user.post("/", description="添加新用户")
 async def add_user(new_user: UserSchema):
     # 查看是否已经存在
     user_exist = await User.get_or_none(number=new_user.number)
-    # 如果用户不存在，则创建新用户
     if user_exist is not None:
         raise HTTPException(status_code=400, detail="User with this phone number already exists.")
     # 验证码 TODO
@@ -22,6 +29,20 @@ async def add_user(new_user: UserSchema):
     # 插入数据库
     await User.create(**new_user.dict())
     return {"data": "插入成功"}
+
+
+@user.post('/login', description="登录")
+async def login_user(new_user: UserSchema):
+    # 查看是否已经存在
+    user_exist = await User.get_or_none(number=new_user.number)
+    if user_exist is None:
+        raise HTTPException(status_code=404, detail="User not found.")
+    if new_user.password != user_exist.password:
+        raise HTTPException(status_code=400, detail="The password is incorrect")
+    # 验证码 TODO
+
+    return {"data": "登陆成功"}
+
 
 
 @user.put("/", description="修改用户信息")
