6ecd16718ef6a59cd41f65d99e4c5b4c2f84511d
Lxx1735827 <870375416@qq.com>
2024-10-27 19:49:21 +0800

    test

--- src/main.py
+++ src/main.py
@@ -32,5 +32,6 @@ register_tortoise(
 # 将 static 目录中的文件作为静态文件提供
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
