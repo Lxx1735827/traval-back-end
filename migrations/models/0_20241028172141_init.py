e5903e8a76c99bf110feb0ae75fb2a48f3c72d8a
Lxx1735827 <870375416@qq.com>
2024-10-28 17:28:34 +0800

    setting

--- migrations/models/0_20241028172141_init.py
+++ migrations/models/0_20241028172141_init.py
@@ -3,15 +3,40 @@ from tortoise import BaseDBAsyncClient
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
-        CREATE TABLE IF NOT EXISTS `site` (
+        CREATE TABLE IF NOT EXISTS `restaurant` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(20) NOT NULL  COMMENT '景点名字',
+    `name` VARCHAR(100) NOT NULL  COMMENT '餐厅名字',
     `city` VARCHAR(20) NOT NULL  COMMENT '城市',
-    `description` VARCHAR(1000) NOT NULL  COMMENT '景点描述',
-    `picture` VARCHAR(40) NOT NULL  COMMENT '景点图片' DEFAULT 'static/site/默认图片.png',
-    `location` VARCHAR(40) NOT NULL  COMMENT '景点地址',
+    `image` VARCHAR(255) NOT NULL  COMMENT '餐厅图片' DEFAULT 'static/restaurant/默认图片.png',
+    `location` VARCHAR(500) NOT NULL  COMMENT '餐厅地址',
+    `telephone` VARCHAR(100)   COMMENT '餐厅联系电话',
     `longitude` DECIMAL(9,6) NOT NULL  COMMENT '经度',
-    `latitude` DECIMAL(9,6) NOT NULL  COMMENT '纬度'
+    `latitude` DECIMAL(9,6) NOT NULL  COMMENT '纬度',
+    `type` VARCHAR(50) NOT NULL  COMMENT '对象类型' DEFAULT '餐厅'
+) CHARACTER SET utf8mb4;
+CREATE TABLE IF NOT EXISTS `review` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `entity_id` INT NOT NULL  COMMENT '景点或餐厅ID',
+    `entity_type` VARCHAR(50) NOT NULL  COMMENT '类型',
+    `user_id` INT NOT NULL  COMMENT '用户ID',
+    `content` LONGTEXT   COMMENT '评论内容',
+    `created_at` DATETIME(6) NOT NULL  COMMENT '记录时间'
+) CHARACTER SET utf8mb4;
+CREATE TABLE IF NOT EXISTS `site` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL  COMMENT '景点名字',
+    `city` VARCHAR(20) NOT NULL  COMMENT '城市',
+    `description` VARCHAR(2000)   COMMENT '景点简介',
+    `picture` VARCHAR(255) NOT NULL  COMMENT '景点图片' DEFAULT 'static/site/默认图片.png',
+    `location` VARCHAR(255) NOT NULL  COMMENT '景点地址',
+    `telephone` VARCHAR(255)   COMMENT '景点联系电话',
+    `time_reference` VARCHAR(255)   COMMENT '景点用时参考',
+    `transport` LONGTEXT   COMMENT '交通参考',
+    `ticket` VARCHAR(3000)   COMMENT '景点门票',
+    `open_time` VARCHAR(2000)   COMMENT '景点开放时间',
+    `longitude` DECIMAL(9,6) NOT NULL  COMMENT '经度',
+    `latitude` DECIMAL(9,6) NOT NULL  COMMENT '纬度',
+    `type` VARCHAR(50) NOT NULL  COMMENT '对象类型' DEFAULT '景点'
 ) CHARACTER SET utf8mb4;
 CREATE TABLE IF NOT EXISTS `user` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
