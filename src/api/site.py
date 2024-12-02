2559823c0238036b8d99b6898032991287f1020d
u18942889323/u18942889323 <3105969766@qq.com>
2024-12-02 21:49:51 +0800

    新添加了search的高级搜法

--- src/api/site.py
+++ src/api/site.py
@@ -1,10 +1,24 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, File, UploadFile, HTTPException
+from src.schema import *
 from tortoise.exceptions import DoesNotExist
-from src.utils.aprioriutils import *
-from src.setting import *
 import math
-import random
-import time
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+from scipy.linalg import norm
+import re
+from tortoise import Tortoise
+
+# LLM调用
+from sparkai.llm.llm import ChatSparkLLM, ChunkPrintHandler
+from sparkai.core.messages import ChatMessage
+#星火认知大模型Spark Max的URL值，其他版本大模型URL值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
+SPARKAI_URL = 'wss://spark-api.xf-yun.com/v3.5/chat'
+#星火认知大模型调用秘钥信息，请前往讯飞开放平台控制台（https://console.xfyun.cn/services/bm35）查看
+SPARKAI_APP_ID = 'f5f377c0'
+SPARKAI_API_SECRET = 'YjFiNWZmYTQwMGMyZDZhOGIwMzk0NDYw'
+SPARKAI_API_KEY = '3d7cdb75e47c594d01a98d691b950aa2'
+#星火认知大模型Spark Max的domain值，其他版本大模型domain值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
+SPARKAI_DOMAIN = 'generalv3.5'
 
 site = APIRouter()
 
@@ -26,24 +40,22 @@ async def get_sites():
 @site.get('/{site_id}', description="根据site的id获取site的所有信息")
 async def get_site_by_id(site_id: int):
     try:
-        print(1)
-        sites = await Site.get(id=site_id)
+        site = await Site.get(id=site_id)
         site_found = {
-            "id": sites.id,
-            "name": sites.name,
-            "city": sites.city,
-            "description": sites.description,
-            "picture": sites.picture,
-            "location": sites.location,
-            "telephone": sites.telephone,
-            "time_reference": sites.time_reference,
-            "transport": sites.transport,
-            "ticket": sites.ticket,
-            "open_time": sites.open_time,
-            "longitude": sites.longitude,
-            "latitude": sites.latitude
+            "id": site.id,
+            "name": site.name,
+            "city": site.city,
+            "description": site.description,
+            "picture": site.picture,
+            "location": site.location,
+            "telephone": site.telephone,
+            "time_reference": site.time_reference,
+            "transport": site.transport,
+            "ticket": site.ticket,
+            "open_time": site.open_time,
+            "longitude": site.longitude,
+            "latitude": site.latitude
         }
-        print(2)
         return {"data": site_found}
     except DoesNotExist:
         raise HTTPException(status_code=404, detail="Site not found")
@@ -125,35 +137,6 @@ async def user_sites(user_number: str):
     return {"data": site_list}
 
 
-@site.get('/user/recommend/{user_number}', description='推荐算法')
-async def user_recommend_sites(user_number: str):
-    # 查找用户是否存在
-    user_exist = await User.get_or_none(number=user_number).prefetch_related('sites')
-    if user_exist is None:
-        raise HTTPException(status_code=400, detail="用户不存在")
-    sites = await user_exist.sites.all()
-    site_ids1 = [site_it.id for site_it in sites]
-    sites = await user_exist.sites.all()
-    site_ids2 = [site_it.id for site_it in sites]
-    sites = list(set(site_ids1).union(set(site_ids2)))
-    random.seed(time.time())
-    random.shuffle(sites)
-    site_lists = set()
-    for site_id in sites:
-        return_sites = await SiteRelationship.filter(site_from_ids__icontains=site_id).values("site_to_ids")
-        for return_site in return_sites:
-            site_list = return_site["site_to_ids"].split("*")[:-1]
-            site_list = set(site_list)
-            site_lists = site_lists.union(site_list)
-            if len(site_lists) > 7:
-                break
-    for i in range(len(site_lists)-1, 7):
-        site_lists.add(SITE[i])
-    site_lists = [int(site_) for site_ in site_lists]
-
-    return {"data": site_lists}
-
-
 @site.delete("/user-site", description="用户取消收藏")
 async def user_sites(user_number: str, site_id):
     # 查找用户是否存在
@@ -171,23 +154,83 @@ async def user_sites(user_number: str, site_id):
 
     return {'data': "取消收藏成功"}
 
-
-@site.get("/search/{key}", description="搜索景点名字，返回最相似的")
+@site.get("/search/{key}", description="搜索景点名字，返回最相似的7个")
 async def search_site(key: str):
     try:
-        sites = await Site.filter(name__icontains=key).limit(7)  # 使用first()取最匹配的记录
+        key_parts = list(key)
+        regex_pattern = ".*" + ".*".join(map(re.escape, key_parts)) + ".*"
+        conn = Tortoise.get_connection("default")
+        sites = await conn.execute_query('SELECT * FROM site WHERE name REGEXP %s', regex_pattern)
+        print(sites)
+        # site = await Site.filter(name__posix_regex=regex_pattern)#.limit(7)  # 使用first()取最匹配的记录
         if not sites:
             raise HTTPException(status_code=404, detail="Site not found")
+        real_sites = sites[1]
+        # filtered_sites = [site for site in real_sites if site['description'] != '暂无介绍']
+        result = [{"id": site["id"], "name": site["name"]} for site in real_sites[:7]]
 
-        result = [{"id": site.id, "name": site.name} for site in sites]
         return {"data": result}
 
     except DoesNotExist:
         raise HTTPException(status_code=404, detail="Site not found")
 
+@site.get("/largesearch/{key}", description="搜索景点名字，返回最相似的")
+async def search_site(key: str):
+    try:
+        keywords = semantic_split(key)
+        rough = []
+        for keyword in keywords:
+            temp = await Site.filter(name__icontains=keyword)
+            rough.extend(temp)
+        unique_rough = list({site.id: site for site in rough}.values())
 
-@site.get("/timer/{key}", description="搜索景点名字，返回最相似的")
-async def search_site(key: int):
-    if key == 10:
-        await insert_data()
-    return {"data": "成功"}
+        similarities = [(site, tf_similarity(site.name, key)) for site in unique_rough]
+
+        # 按相似度从大到小排序
+        sorted_sites = [{"id":site.id, "name": site.name, "picture": site.picture, "location": site.location} for site, _ in sorted(similarities, key=lambda x: x[1], reverse=True)]
+
+        # 限制返回最多35个site
+        return {"data": sorted_sites[:35]}
+
+    except DoesNotExist:
+        raise HTTPException(status_code=404, detail="Site not found")
+
+def semantic_split(key: str):
+    spark = ChatSparkLLM(
+        spark_api_url=SPARKAI_URL,
+        spark_app_id=SPARKAI_APP_ID,
+        spark_api_key=SPARKAI_API_KEY,
+        spark_api_secret=SPARKAI_API_SECRET,
+        spark_llm_domain=SPARKAI_DOMAIN,
+        streaming=False,
+    )
+    messages = [
+        ChatMessage(role="system",
+                    content="你是一个语义分析助手，专注于将中文短语拆分成有意义的最小单元，保留核心名词，并尽可能涵盖上下层次的含义。"),
+        ChatMessage(
+            role="user",
+            content=f"将搜索词'{key}'拆分成有意义的最小单元，保留核心名词或短语，并尽量涵盖整体及子层次的含义。"
+                    f"输出格式为：'keywords: xxx, xxx, xxx'。例如："
+                    f"1. '成都大熊猫基地'拆分为'成都, 熊猫, 基地'；"
+                    f"2. '四川大学江安校区'拆分为'四川大学, 江安校区, 四川, 大学, 江安'。"
+        )
+    ]
+    handler = ChunkPrintHandler()
+    a = spark.generate([messages], callbacks=[handler])
+    # 获取包含 city 和 keywords 的文本
+    text = a.generations[0][0].text  # 从数据中提取文本
+    keywords = text.strip("'").replace('keywords:', '').strip()
+    print(f"Keywords:{keywords}")
+    result = [item.strip() for item in keywords.split(',')]
+    return result
+
+def tf_similarity(s1, s2):
+    def add_space(s):
+        return ' '.join(list(s))
+
+    s1, s2 = add_space(s1), add_space(s2) #在字中间加上空格
+    cv = CountVectorizer(tokenizer=lambda s: s.split()) #转化为TF矩阵
+    corpus = [s1, s2]
+    vectors = cv.fit_transform(corpus).toarray() #计算TF系数
+    return np.dot(vectors[0], vectors[1]) / (norm(vectors[0]) * norm(vectors[1]))
+
