e3ab7e174e655c8280d88f6dfc8401a5a865dcbf
Lxx1735827 <870375416@qq.com>
2025-01-11 20:35:41 +0800

    笔记模块

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from src.api.route import route
 from src.api.blockchain import bc
 from src.api.language import language
 from src.api.friend import friend
+from src.api.note import note
 from fastapi.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 from fastapi.staticfiles import StaticFiles
@@ -26,6 +27,7 @@ app.include_router(route, prefix="/route", tags=["route"])
 app.include_router(bc, prefix="/bc", tags=["bc"])
 app.include_router(language, prefix="/language", tags=["language"])
 app.include_router(friend, prefix="/friend", tags=["friend"])
+app.include_router(note, prefix="/note", tags=["note"])
 
 # 定义一个继承自BaseHTTPMiddleware的自定义中间件类
 class CustomMiddleware(BaseHTTPMiddleware):
