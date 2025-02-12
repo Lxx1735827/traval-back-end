3d25a06cdde09d0a82dae1dacd974cc3761d0c0f
Lxx1735827 <870375416@qq.com>
2025-02-12 18:02:35 +0800

    note笔记

--- src/api/note.py
+++ src/api/note.py
@@ -170,6 +170,8 @@ async def note_reviews(note_id: int):
     reviews = await NoteReview.filter(id=note_id).all()
     data = []
     for review in reviews:
+        print(type(review.user_number))
+        print(review.user_number)
         user = await User.filter(id=review.user_number).first()
         data.append({
             "id": review.id,
