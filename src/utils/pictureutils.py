3e9c3d1dc0be213da1fa0de2ab9741c94ea7e9cb
Lxx1735827 <870375416@qq.com>
2025-02-20 15:25:26 +0800

    字体修改

--- src/utils/pictureutils.py
+++ src/utils/pictureutils.py
@@ -14,7 +14,7 @@ async def day_routes(pictures: List[str], info: str):
     :param info: 路径信息
     :return:
     """
-    plt.rcParams['font.family'] = 'WenQuanYi Micro Hei'
+    plt.rcParams['font.family'] = 'SimHei'  # 替换为你选择的字体
     plt.rcParams["axes.unicode_minus"] = False  # 正常显示负号
 
     info_map = json.loads(info)
