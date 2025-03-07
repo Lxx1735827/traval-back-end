8a2bf346c210f97598a5ac192c6d0e5e3b7fa4e3
Lxx1735827 <870375416@qq.com>
2025-03-07 16:44:55 +0800

    历史足迹

--- src/api/user.py
+++ src/api/user.py
@@ -352,11 +352,14 @@ async def get_friend_ask(user_number: str):
 
 @user.get("/footprint/{user_number}", description="生成历史足迹")
 async def get_footprint(user_number: str):
-    user_exist = await User.get_or_none(number=user_number)
+    user_exist = await User.get_or_none(number=user_number).prefetch_related('visit_sites')  # 加载相关的 visit_sites
     if user_exist is None:
         raise HTTPException(status_code=404, detail="User with this phone number does not exist.")
     sites = user_exist.visit_sites
-    print(len(sites))
+    print(len(sites))  # 现在可以安全地打印 sites 的长度
+    for site in sites:
+        print(site.id)  # 打印每个 site 的 id
+
 
 
 
