a053473f40aba001c971964b9dff956d1e41ac5a
Lxx1735827 <870375416@qq.com>
2025-03-30 18:41:36 +0800

    笔记推送

--- src/api/note.py
+++ src/api/note.py
@@ -203,7 +203,7 @@ async def note_review(number: str, note_id: int, content: str, create_time: str)
 @note.post("/recommend", description="笔记推送")
 async def note_recommend(number: str):
     notes = await Note.filter().all()
-    notes = notes[:10]
+    notes = notes[-10:]
     data = []
     for note_ in notes:
         picture = note_.picture.split("##")[0]
